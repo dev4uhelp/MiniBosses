547bcd310929db5b9a81f290d2275a344fd55180
jasonwynn10 <jasonwynn10@gmail.com>
2018-07-03 17:41:05 -0400

    Update plugin to logically working state I say logically working because my IDE says it's okay but it's untested

--- src/MiniBosses/Boss.php
+++ src/MiniBosses/Boss.php
@@ -1,192 +1,184 @@
 <?php
-
 namespace MiniBosses;
 
+use pocketmine\entity\Creature;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\item\Item;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\network\mcpe\protocol\AddEntityPacket;
+use pocketmine\network\mcpe\protocol\AddPlayerPacket;
+use pocketmine\network\mcpe\protocol\MobEquipmentPacket;
 use pocketmine\Player;
 use pocketmine\utils\UUID;
-use pocketmine\entity\Creature;
-use pocketmine\nbt\tag\ListTag;
-use pocketmine\nbt\tag\IntTag;
-use pocketmine\nbt\tag\FloatTag;
-use pocketmine\nbt\tag\StringTag;
-use pocketmine\nbt\tag\ByteTag;
-use pocketmine\nbt\tag\DoubleTag;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\level\Position;
-use pocketmine\item\Item;
-use pocketmine\network\protocol\AddEntityPacket;
-use pocketmine\network\protocol\AddPlayerPacket;
-use pocketmine\network\protocol\MobEquipmentPacket;
 
-class Boss extends Creature{
-	
+class Boss extends Creature {
+
+	const NETWORK_ID = 1000;
 	public $networkId = 32;
+	/** @var Player|null */
 	public $target;
 	public $spawnPos;
 	public $attackDamage = 1;
 	public $attackRate = 10;
 	public $attackDelay = 0;
 	public $speed;
-	public $drops = array();
+	/** @var Item[][] */
+	public $drops = [];
 	public $respawnTime;
 	public $skin;
 	public $heldItem;
 	public $range;
 	public $knockbackTicks = 0;
 	public $scale;
-	const NETWORK_ID = 1000;
-	
-	public function __construct($chunk,$nbt){
-		parent::__construct($chunk,$nbt);
-		$this->networkId = (int)$this->namedtag["networkId"];
-		$this->range = $this->namedtag["range"];
-		$this->spawnPos = new Position($this->namedtag["spawnPos"][0],$this->namedtag["spawnPos"][1],$this->namedtag["spawnPos"][2],$this->level);
-		$this->attackDamage = $this->namedtag["attackDamage"];
-		$this->attackRate = $this->namedtag["attackRate"];
-		$this->speed = $this->namedtag["speed"];
-		$this->scale = $this->namedtag["scale"] ?? 1;
-		if($this->namedtag["drops"] !== ""){
-			foreach(explode(' ',$this->namedtag["drops"]) as $item){
-				$item = explode(';',$item);
-				$this->drops[] = [Item::get($item[0],$item[1] ?? 0,$item[2] ?? 1,$item[3] ?? ""),$item[4] ?? 100];
+	/** @var Main */
+	private $plugin;
+
+	public function __construct($chunk, $nbt) {
+		parent::__construct($chunk, $nbt);
+		$this->networkId = (int) $this->namedtag->getInt("networkId");
+		$this->range = $this->namedtag->getFloat("range");
+		$this->spawnPos = new Position($this->namedtag->getListTag("spawnPos")[0], $this->namedtag->getListTag("spawnPos")[1], $this->namedtag->getListTag("spawnPos")[2], $this->level);
+		$this->attackDamage = $this->namedtag->getFloat("attackDamage");
+		$this->attackRate = $this->namedtag->getInt("attackRate");
+		$this->speed = $this->namedtag->getInt("speed");
+		$this->scale = $this->namedtag->getFloat("scale") ?? 1;
+		if($this->namedtag->getString("drops") !== "") {
+			foreach(explode(' ', $this->namedtag->getString("drops")) as $item) {
+				$item = explode(';', $item);
+				$this->drops[] = [Item::get($item[0], $item[1] ?? 0, $item[2] ?? 1, $item[3] ?? ""), $item[4] ?? 100];
 			}
 		}
-		$this->respawnTime = $this->namedtag["respawnTime"];
-		$this->skin = $this->namedtag["skin"];
-		if($this->namedtag["heldItem"] !== ""){
-			$heldItem = explode(';',$this->namedtag["heldItem"]);
-			$this->heldItem = Item::get($heldItem[0],$heldItem[1] ?? 0,$heldItem[2] ?? 1,$heldItem[3] ?? "");
+		$this->respawnTime = $this->namedtag->getInt("respawnTime");
+		$this->skin = $this->namedtag->getString("skin");
+		if($this->namedtag->getString("heldItem") !== "") {
+			$heldItem = explode(';', $this->namedtag->getString("heldItem"));
+			$this->heldItem = Item::get($heldItem[0], $heldItem[1] ?? 0, $heldItem[2] ?? 1, $heldItem[3] ?? "");
 		}else $this->heldItem = Item::get(0);
 	}
-	
-	public function initEntity(){
+
+	public function initEntity() : void {
 		$this->plugin = $this->server->getPluginManager()->getPlugin("MiniBosses");
-        parent::initEntity();
-        $this->dataProperties[self::DATA_FLAG_NO_AI] = [self::DATA_TYPE_BYTE, 1];
-        $this->dataProperties[self::DATA_SCALE] = [self::DATA_TYPE_FLOAT, $this->namedtag["scale"]];
-		if(isset($this->namedtag->maxHealth)){
-			parent::setMaxHealth($this->namedtag["maxHealth"]);
-			$this->setHealth($this->namedtag["maxHealth"]);
-		}else{
+		parent::initEntity();
+		$this->setImmobile(true);
+		$this->setScale($this->namedtag->getFloat("scale"));
+		if($this->namedtag->getInt("maxHealth") !== null) {
+			parent::setMaxHealth($this->namedtag->getInt("maxHealth"));
+			$this->setHealth($this->namedtag->getInt("maxHealth"));
+		}else {
 			$this->setMaxHealth(20);
 			$this->setHealth(20);
 		}
-    }
-	
-	public function getName(){
-		return $this->getNameTag();
 	}
-	
-	public function spawnTo(Player $player){
+
+	public function setMaxHealth($health) : void {
+		$this->namedtag->setInt("maxHealth", $health);
+		parent::setMaxHealth($health);
+	}
+
+	public function spawnTo(Player $player) : void {
 		parent::spawnTo($player);
-		if($this->networkId === 63){
+		if($this->networkId === 63) {
 			$pk = new AddPlayerPacket();
 			$pk->uuid = UUID::fromData($this->getId(), $this->skin, $this->getNameTag());
 			$pk->username = $this->getName();
-			$pk->eid = $this->getId();
-			$pk->x = $this->x;
-			$pk->y = $this->y;
-			$pk->z = $this->z;
-			$pk->speedX = $this->motionX;
-			$pk->speedY = $this->motionY;
-			$pk->speedZ = $this->motionZ;
+			$pk->entityRuntimeId = $this->getId();
+			$pk->position = $this->asPosition();
+			$pk->motion = $this->motion;
 			$pk->yaw = $this->yaw;
 			$pk->pitch = $this->pitch;
 			$pk->item = $this->heldItem;
-			$pk->metadata = $this->dataProperties;
+			$pk->metadata = $this->propertyManager->getAll();
 			$player->dataPacket($pk);
-		}else{
+		}else {
 			$pk = new AddEntityPacket();
-			$pk->eid = $this->getID();
+			$pk->entityRuntimeId = $this->getID();
 			$pk->type = $this->networkId;
-			$pk->x = $this->x;
-			$pk->y = $this->y;
-			$pk->z = $this->z;
-			$pk->speedX = 0;
-			$pk->speedY = 0;
-			$pk->speedZ = 0;
+			$pk->position = $this->asPosition();
+			$pk->motion = new Vector3();
 			$pk->yaw = $this->yaw;
 			$pk->pitch = $this->pitch;
-			$pk->metadata = $this->dataProperties;
+			$pk->metadata = $this->propertyManager->getAll();
 			$player->dataPacket($pk);
-			if($this->heldItem->getId() > 0){
+			if($this->heldItem->getId() > 0) {
 				$pk = new MobEquipmentPacket();
-				$pk->eid = $this->getId();
+				$pk->entityRuntimeId = $this->getId();
 				$pk->item = $this->heldItem;
-				$pk->slot = 0;
-				$pk->selectedSlot = 0;
+				$pk->inventorySlot = 0;
+				$pk->hotbarSlot = 0;
 				$player->dataPacket($pk);
 			}
 		}
 	}
-	
-	public function setMaxHealth($health){
-		$this->namedtag->maxHealth = new IntTag("maxHealth",$health);
-		parent::setMaxHealth($health);
+
+	public function getName() : string {
+		return $this->getNameTag();
 	}
-	
-	public function saveNBT(){
-        parent::saveNBT();
-		$this->namedtag->maxHealth = new IntTag("maxHealth",$this->getMaxHealth());
-		$this->namedtag->spawnPos = new ListTag("spawnPos", [
-                new DoubleTag("", $this->spawnPos->x),
-                new DoubleTag("", $this->spawnPos->y),
-                new DoubleTag("", $this->spawnPos->z)
-            ]);
-		$this->namedtag->range = new FloatTag("range",$this->range);
-		$this->namedtag->attackDamage = new FloatTag("attackDamage",$this->attackDamage);
-		$this->namedtag->networkId = new IntTag("networkId",$this->networkId);
-		$this->namedtag->attackRate = new IntTag("attackRate",$this->attackRate);
-		$this->namedtag->speed = new FloatTag("speed",$this->speed);
+
+	public function saveNBT() : void {
+		parent::saveNBT();
+		$this->namedtag->setInt("maxHealth", $this->getMaxHealth());
+		$this->namedtag->setTag(new ListTag("spawnPos", [new DoubleTag("", $this->spawnPos->x), new DoubleTag("", $this->spawnPos->y), new DoubleTag("", $this->spawnPos->z)]));
+		$this->namedtag->setFloat("range", $this->range);
+		$this->namedtag->setFloat("attackDamage", $this->attackDamage);
+		$this->namedtag->setInt("networkId", $this->networkId);
+		$this->namedtag->setInt("attackRate", $this->attackRate);
+		$this->namedtag->setInt("speed", $this->speed);
 		$drops2 = [];
-		foreach($this->drops as $drop) $drops2[] = $drop[0]->getId().";".$drop[0]->getDamage().";".$drop[0]->getCount().";".$drop[0]->getCompoundTag().";".$drop[1];
-		$this->namedtag->drops = new StringTag("drops",implode(' ',$drops2));
-		$this->namedtag->respawnTime = new IntTag("respawnTime",$this->respawnTime);
-		$this->namedtag->skin = new StringTag("skin",$this->skin);
-		$this->namedtag->heldItem = new StringTag("heldItem",($this->heldItem instanceof Item ? $this->heldItem->getId().";".$this->heldItem->getDamage().";".$this->heldItem->getCount().";".$this->heldItem->getCompoundTag() : ""));
-		$this->namedtag->scale = new IntTag("scale", $this->scale);
+		foreach($this->drops as $drop) {
+			$drops2[] = $drop[0]->getId().";".$drop[0]->getDamage().";".$drop[0]->getCount().";".$drop[0]->getCompoundTag().";".$drop[1];
+		}
+		$this->namedtag->setString("drops", implode(' ', $drops2));
+		$this->namedtag->setInt("respawnTime", $this->respawnTime);
+		$this->namedtag->setString("skin", $this->skin);
+		$this->namedtag->setString("heldItem", ($this->heldItem instanceof Item ? $this->heldItem->getId().";".$this->heldItem->getDamage().";".$this->heldItem->getCount().";".$this->heldItem->getCompoundTag() : ""));
+		$this->namedtag->setFloat("scale", $this->scale);
 	}
-	
-	public function onUpdate($currentTick){
-		if($this->knockbackTicks > 0) $this->knockbackTicks--;
-		if(($player = $this->target) && $player->isAlive()){
-			if($this->distanceSquared($this->spawnPos) > $this->range){
+
+	public function onUpdate($currentTick) : bool {
+		if($this->knockbackTicks > 0) {
+			$this->knockbackTicks--;
+		}
+		if(($player = $this->target) and $player->isAlive()) {
+			if($this->distanceSquared($this->spawnPos) > $this->range) {
 				$this->setPosition($this->spawnPos);
 				$this->setHealth($this->getMaxHealth());
 				$this->target = null;
-			}else{
-				if(!$this->onGround){
-					if($this->motionY > -$this->gravity * 4){
-						$this->motionY = -$this->gravity * 4;
-					}else{
-						$this->motionY -= $this->gravity;
+			}else {
+				if(!$this->onGround) {
+					if($this->motion->y > -$this->gravity * 4) {
+						$this->motion->y = -$this->gravity * 4;
+					}else {
+						$this->motion->y -= $this->gravity;
 					}
-					$this->move($this->motionX, $this->motionY, $this->motionZ);
-				}elseif($this->knockbackTicks > 0){
-					
-				}else{
-					$x = $player-> x - $this->x;
-					$y = $player-> y - $this->y;
-					$z = $player-> z - $this->z;
-					if($x ** 2 + $z ** 2 < 0.7){
-						$this->motionX = 0;
-						$this->motionZ = 0;
-					}else{
+					$this->move($this->motion->x, $this->motion->y, $this->motion->z);
+				}elseif($this->knockbackTicks > 0) {
+
+				}else {
+					$x = $player->x - $this->x;
+					$y = $player->y - $this->y;
+					$z = $player->z - $this->z;
+					if($x ** 2 + $z ** 2 < 0.7) {
+						$this->motion->x = 0;
+						$this->motion->z = 0;
+					}else {
 						$diff = abs($x) + abs($z);
-						$this->motionX = $this->speed * 0.15 * ($x / $diff);
-						$this->motionZ = $this->speed * 0.15 * ($z / $diff);
+						$this->motion->x = $this->speed * 0.15 * ($x / $diff);
+						$this->motion->z = $this->speed * 0.15 * ($z / $diff);
 					}
-					$this->yaw = rad2deg(atan2(-$x,$z));
-					if($this->networkId === 53){#enderdragon
-						$this->yaw+=180;
+					$this->yaw = rad2deg(atan2(-$x, $z));
+					if($this->networkId === 53) {#enderdragon
+						$this->yaw += 180;
 					}
 					$this->pitch = rad2deg(atan(-$y));
-					$this->move($this->motionX, $this->motionY, $this->motionZ);
-					if($this->distanceSquared($this->target) < $this->scale && $this->attackDelay++ > $this->attackRate){
+					$this->move($this->motion->x, $this->motion->y, $this->motion->z);
+					if($this->distanceSquared($this->target) < $this->scale and $this->attackDelay++ > $this->attackRate) {
 						$this->attackDelay = 0;
 						$ev = new EntityDamageByEntityEvent($this, $this->target, EntityDamageEvent::CAUSE_ENTITY_ATTACK, $this->attackDamage);
-						$player->attack($ev->getFinalDamage(), $ev);
+						$player->attack($ev);
 					}
 				}
 			}
@@ -195,30 +187,32 @@ class Boss extends Creature{
 		parent::onUpdate($currentTick);
 		return !$this->closed;
 	}
-	
-	public function attack($damage, EntityDamageEvent $source){
-		if(!$source->isCancelled() && $source instanceof EntityDamageByEntityEvent){
+
+	public function attack(EntityDamageEvent $source) : void {
+		if(!$source->isCancelled() and $source instanceof EntityDamageByEntityEvent) {
 			$dmg = $source->getDamager();
-			if($dmg instanceof Player){
+			if($dmg instanceof Player) {
 				$this->target = $dmg;
-				parent::attack($damage, $source);
-				$this->motionX = ($this->x - $dmg->x) * 0.19;
-				$this->motionY = 0.5;
-				$this->motionZ = ($this->z - $dmg->z) * 0.19;
+				parent::attack($source);
+				$this->motion->x = ($this->x - $dmg->x) * 0.19;
+				$this->motion->y = 0.5;
+				$this->motion->z = ($this->z - $dmg->z) * 0.19;
 				$this->knockbackTicks = 10;
 			}
-        }
-    }
-	
-	public function kill(){
-		parent::kill();
-		$this->plugin->respawn($this->getNameTag(),$this->respawnTime);
+		}
 	}
-	
-	public function getDrops(){
-		$drops = array();
-		foreach($this->drops as $drop){
-			if(mt_rand(1,100) <= $drop[1]) $drops[] = $drop[0];
+
+	public function kill() : void {
+		parent::kill();
+		$this->plugin->respawn($this->getNameTag(), $this->respawnTime);
+	}
+
+	public function getDrops() : array {
+		$drops = [];
+		foreach($this->drops as $drop) {
+			if(mt_rand(1, 100) <= $drop[1]) {
+				$drops[] = $drop[0];
+			}
 		}
 		return $drops;
 	}
